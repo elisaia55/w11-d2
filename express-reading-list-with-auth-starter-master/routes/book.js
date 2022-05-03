e8d59eba85bd5074724a54e7cd5e0d32eaead72d
Andrew Stilinovic <stili87@gmail.com>
2022-05-03 15:32:30 -0500

    before using sessions

--- express-reading-list-with-auth-starter-master/routes/book.js
+++ express-reading-list-with-auth-starter-master/routes/book.js
@@ -2,14 +2,10 @@
 const express = require('express');
 const csrf = require('csurf');
 const { check, validationResult } = require('express-validator');
-
-const db = require('./db/models');
-
+const db = require('../db/models');
 const router = express.Router();
+const { csrfProtection, asyncHandler } = require('./utils');
 
-const csrfProtection = csrf({ cookie: true });
-
-const asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
 
 router.get('/', asyncHandler(async (req, res) => {
   const books = await db.Book.findAll({ order: [['title', 'ASC']] });
